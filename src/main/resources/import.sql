b575a0f98157927476ad390b23a77d193dee0062
murugek <Kumaravel.Murugesan@arcadiagroup.co.uk>
2019-07-29 09:26:14 +0100

    docker

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 insert into COUNTRY(created, description, last_update, mnemonic) values ( now(1), 'United States of America', now(1), 'USA' );
+insert into COUNTRY(created, description, last_update, mnemonic) values ( now(1), 'United Kingdom', now(1), 'UK' );
